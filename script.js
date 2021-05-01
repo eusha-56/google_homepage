ea9a480c0a441858d5161df7d29885b1eff8584a
eusha-56 <yafaseusha02@gmail.com>
2021-05-01 17:20:38 +0600

    updated search feature

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var search = document.getElementById('search_bar');
 
 search.addEventListener("keyup",function(event){
 	if (event.keyCode === 13) {
-		window.open('https://www.google.com/search?q='+search.value);
+		window.open('https://www.google.com/search?q='+search.value,"_self");
 	}
 });
