521ffd6205c98516ffcf500e095018ccd9e70085
eusha-56 <yafaseusha02@gmail.com>
2021-05-01 15:55:45 +0600

    added search feature

---
+++ script.js
@@ -0,0 +1,7 @@
+var search = document.getElementById('search_bar');
+
+search.addEventListener("keyup",function(event){
+	if (event.keyCode === 13) {
+		window.open('https://www.google.com/search?q='+search.value);
+	}
+});
