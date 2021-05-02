5c8684bf51c15e471e10372f4faf30be64023579
eusha-56 <yafaseusha02@gmail.com>
2021-05-02 13:58:45 +0600

    various bugs fixed

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ search.addEventListener("keyup",function(event){
 		Search()
 	}
 });
+if (window.innerHeight>600) {
+	var bottom_margin = (window.innerHeight-document.getElementById("top").offsetHeight-document.getElementById("bottom").offsetHeight)+"px";
+	document.getElementById("bottom").style.marginTop = bottom_margin;
+}else{
+	document.getElementById("bottom").style.marginTop = "100px";
+}
